91f08153db30ad7d1a97b93b41e18a45cb69c27d
AINDRILA GARAI <110183041+AindrilaGarai@users.noreply.github.com>
2023-01-12 20:12:02 +0530

    Update exercise3.R

--- exercise3.R
+++ exercise3.R
@@ -24,3 +24,43 @@ new <- rotate(d,45)
 
 p3 <- ggplot(new, aes(x = X1, y = X2)) + geom_point(alpha = .5) + geom_density_2d()
 p3
+
+library(matlib)
+library(mvtnorm)
+library(ggplot2)
+library(pracma)
+sigma <- matrix(c(4,2,2,3), ncol = 2, nrow = 2) 
+
+## Mean
+mu <- c(1, 2)
+n <- 1000
+set.seed(123)
+x <- rmvnorm(n = n, mean = mu, sigma = sigma)
+d <- data.frame(x)
+p2 <- ggplot(d, aes(x = X1, y = X2)) + geom_point(alpha = .5) + geom_density_2d()
+p2
+
+rotation <- function(degree)
+{
+  t <- (pi * degree) / 180
+  data <- c(cos(t),sin(t),-sin(t),cos(t))
+  ma <- matrix(data,2,2)
+  v <- vector(length = 2)
+  new <- matrix(0,1000,2)
+  for ( i in 1:1000)
+  {
+    a <- c(x[i,1],x[i,2])
+    a <- as.vector(a)
+    y[i] <- round((ma %*% a),2)
+    u[i] <- as.matrix(t(y[i]))
+    new[i,] <- u[i]
+  }
+  return(new)
+}
+dat <- data.frame(rotation(45))
+
+
+
+p3 <- ggplot(dat, aes(x = X1, y = X2)) + geom_point(alpha = .5) + geom_density_2d()
+p3
+
